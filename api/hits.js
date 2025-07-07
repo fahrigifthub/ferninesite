7214cc57c87c48cf5885d240f01cc1bd15475350
fahrigifthub <145030038+fahrigifthub@users.noreply.github.com>
2025-07-07 20:40:07 +0700

    Update hits.js

--- api/hits.js
+++ api/hits.js
@@ -1,6 +1,11 @@
-import { getTotalHit } from '@/utils/hitCounter';
+import { getHits, incrementHit } from '@/lib/hit';
 
-export default function handler(req, res) {
-  const total = getTotalHit();
-  res.status(200).json({ total });
+export default async function handler(req, res) {
+  if (req.method === 'POST') {
+    const total = await incrementHit();
+    return res.status(200).json({ total });
+  } else {
+    const total = await getHits();
+    return res.status(200).json({ total });
+  }
 }
